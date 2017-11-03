17a06de55614f727cd8ad0261e3d145f0ae99d9b
John Haugeland <stonecypher@gmail.com>
2017-11-03 09:08:25 -0700

    stand up toy testing

---
+++ src/tests/general.js
@@ -0,0 +1,17 @@
+
+/* eslint-disable max-len */
+
+import {describe} from 'ava-spec';
+
+// const jssm = require('../../../build/jssm.es5.js');
+
+
+
+
+
+describe('can run specs', async it => {
+
+  it('can run tests', t => t.is(1, 1));
+
+});
+
