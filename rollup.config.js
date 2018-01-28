72eb66ce656e9355db9c8b9ea9789ee64ed1082a
John Haugeland <stonecypher@gmail.com>
2018-01-27 22:49:24 -0800

    getting typescript and rollup going; converting js to ts

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+
+import typescript  from 'rollup-plugin-typescript2';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs    from 'rollup-plugin-commonjs';
+
+const pkg = require('./package');
+
+export default {
+
+  input     : 'src/ts/index.ts',
+//moduleId  : pkg.name,
+
+  output    : {
+    file      : 'build/index.js',
+    format    : 'cjs',
+    name      : 'fslp',
+    sourcemap : true,
+  },
+
+  plugins   : [
+
+    commonjs({
+      namedExports: {
+//      'node_modules/react/index.js'     : ['createElement', 'Component'],
+//      'node_modules/react-dom/index.js' : ['render'],
+      },
+    }),
+
+    nodeResolve({
+      module: true,
+      jsnext: true,
+      browser: true,
+//    extensions: [ '.js', '.json', '.jsx', '.ts', '.tsx' ],
+      extensions: [ '.js', '.json', '.ts' ],
+      preferBuiltins: false
+    }),
+
+    typescript({
+      typescript: require('typescript') // use local version
+    })
+
+  ]
+}
