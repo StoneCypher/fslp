f146ec3d5c2a1dd7ceb2f555338c1cf271e816d8
John Haugeland <stonecypher@gmail.com>
2018-02-03 22:35:01 -0800

    simple tests

--- src/tests/arrow.js
+++ src/tests/arrow.js
@@ -9,7 +9,30 @@ const fslp = require('../../build/fslp.js');
 
 
 
-describe('a->b', async it => {
+describe('basic expressions', async it => {
+
+  const arrows = [
+    { write: '->',  rkind: 'normal' },
+    { write: '~>',  rkind: 'forced' },
+    { write: '=>',  rkind: 'main'   },
+    { write: '<-',  lkind: 'normal' },
+    { write: '<~',  lkind: 'forced' },
+    { write: '<=',  lkind: 'main'   },
+    { write: '<->', rkind: 'normal', lkind: 'normal' },
+    { write: '<~>', rkind: 'forced', lkind: 'forced' },
+    { write: '<=>', rkind: 'main',   lkind: 'main'   }
+  ];
+
+  const pads = ['', ' ', '\t', '\r', '\n', '\r\n', ' \r\n\t\n\n \n '];
+
+  const tpad = [].concat(... pads.map(l => pads.map(r => ({l, r}))));
+
+  const make_test = pad => {
+    const src = `a${pad.l}->${pad.r}b;`;
+    it(src, t => t.is('transition', fslp.parse(src).value[0].term ));
+  }
+
+  tpad.map(pad => make_test(pad));
 
   it('a -> b;', t => t.is('transition', fslp.parse('a->b;').value[0].term ));
 
